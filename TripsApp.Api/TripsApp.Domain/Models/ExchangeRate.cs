d16f9e76257a66168ec4c09243238ebb7cdf306a
Sibusiso Zamani <Sibusiso.Zamani@Euromonitor.com>
2021-12-20 15:03:44 +0200

    Updates domain models

---
+++ TripsApp.Api/TripsApp.Domain/Models/ExchangeRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripsApp.Domain.Models
+{
+    public class ExchangeRate
+    {
+        public int CountryId { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public string Currency { get; set; }
+    }
+}
